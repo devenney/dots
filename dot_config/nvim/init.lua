0eaf424849427675090738f9c2a5fda49ff4b8b4
Brendan Devenney <brendan.devenney@approov.io>
2025-07-09 17:50:22 +0100

    feat: nvim catppuccin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,11 +2,29 @@ vim.g.mapleader = ","
 
 require("config.lazy")
 
+--- Indentation ---
 vim.o.tabstop = 4
 vim.o.expandtab = false
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
+--- Hard Mode ---
+vim.keymap.set("i", "<Up>", ":echoe 'LOL'<CR>")
+vim.keymap.set("i", "<Down>", ":echoe 'LOL'<CR>")
+vim.keymap.set("i", "<Left>", ":echoe 'LOL'<CR>")
+vim.keymap.set("i", "<Right>", ":echoe 'LOL'<CR>")
+
+vim.keymap.set("n", "<Up>", ":echoe 'LOL'<CR>")
+vim.keymap.set("n", "<Down>", ":echoe 'LOL'<CR>")
+vim.keymap.set("n", "<Left>", ":echoe 'LOL'<CR>")
+vim.keymap.set("n", "<Right>", ":echoe 'LOL'<CR>")
+
+vim.keymap.set("v", "<Up>", ":echoe 'LOL'<CR>")
+vim.keymap.set("v", "<Down>", ":echoe 'LOL'<CR>")
+vim.keymap.set("v", "<Left>", ":echoe 'LOL'<CR>")
+vim.keymap.set("v", "<Right>", ":echoe 'LOL'<CR>")
+
+--- Telescope ---
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
