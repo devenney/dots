6e1e48dd7c7b4c98f62f4a0bf705768bb59bf055
Brendan Devenney <brendan.devenney@approov.io>
2025-07-09 15:35:36 +0100

    feat: nvim indentation behaviour

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = ","
 
 require("config.lazy")
 
+vim.o.tabstop = 4
+vim.o.expandtab = false
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
