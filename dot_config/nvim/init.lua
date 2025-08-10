af02928485681c305ebd0cdefd7bb0dce8c59c27
Brendan Devenney <brendan@devenney.io>
2025-08-10 01:48:54 +0100

    feat: consistent indentation

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -16,8 +16,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Indentation
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
 
